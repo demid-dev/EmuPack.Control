fd3ff0e6a743984ef3ff2cefc18b68c27765eb41
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:28:58 +0200

    Update InitializationCommand.cs

--- EmuPack.Control/Models/Commands/InitializationCommand.cs
+++ EmuPack.Control/Models/Commands/InitializationCommand.cs
@@ -5,7 +5,25 @@ using System.Threading.Tasks;
 
 namespace EmuPack.Control.Models.Commands
 {
-    public class InitializationCommand: Command
+    public class InitializationCommand : Command
     {
+        public InitializationCommand()
+        {
+            CommandID = InitializationCommandValues.CommandId;
+            DataLength = NormalizeCommandField(0,
+                CommandValues.DataLengthLength,
+                CommandPadding.Zeroing);
+            CommandString = FormCommand();
+        }
+    }
+
+    static class InitializationCommandValues
+    {
+        static public string CommandId { get; private set; }
+
+        static InitializationCommandValues()
+        {
+            CommandId = "IN";
+        }
     }
 }
