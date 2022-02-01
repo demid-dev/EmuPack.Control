34e210d81a4edabe50039cfa17df160295df3694
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:29:00 +0200

    Update MachineActivityRequestCommand.cs

--- EmuPack.Control/Models/Commands/MachineActivityRequestCommand.cs
+++ EmuPack.Control/Models/Commands/MachineActivityRequestCommand.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using EmuPack.Control.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EmuPack.Control.Models.Commands
@@ -10,5 +12,54 @@ namespace EmuPack.Control.Models.Commands
         public string DataStartNotification { get; private set; }
         public string DrawerStatus { get; private set; }
         public string ClearingWarningsInitiated { get; private set; }
+
+        public MachineActivityRequestCommand(MachineActivityRequestCommandDTO dto)
+        {
+            CommandID = MachineActivityRequestValues.CommandId;
+            DataStartNotification = MachineActivityRequestValues.DataStartNotification;
+            if (dto.DrawerShouldBeLocked)
+                DrawerStatus = MachineActivityRequestValues.DrawerStatusLocked;
+            else
+                DrawerStatus = MachineActivityRequestValues.DrawerStatusUnlocked;
+            if (dto.ClearingShouldBeInitiated)
+                ClearingWarningsInitiated = MachineActivityRequestValues.ClearingInitiated;
+            else
+                ClearingWarningsInitiated = MachineActivityRequestValues.ClearingNotInitiated;
+
+            CommandString = FormCommand();
+        }
+
+        protected override string FormCommand()
+        {
+            List<string> commandFieldsList = new List<string> { DataStartNotification,
+                DrawerStatus, ClearingWarningsInitiated };
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendJoin("", commandFieldsList);
+            DataLength = NormalizeCommandField(stringBuilder.Length.ToString(),
+                CommandValues.DataLengthLength, CommandPadding.Zeroing);
+
+            return base.FormCommand() + stringBuilder.ToString();
+        }
+    }
+
+    static class MachineActivityRequestValues
+    {
+        static public string CommandId { get; private set; }
+        static public string DataStartNotification { get; private set; }
+        static public string DrawerStatusLocked { get; private set; }
+        static public string DrawerStatusUnlocked { get; private set; }
+        static public string ClearingInitiated { get; private set; }
+        static public string ClearingNotInitiated { get; private set; }
+
+        static MachineActivityRequestValues()
+        {
+            CommandId = "MR";
+            DataStartNotification = "D";
+            DrawerStatusLocked = "00";
+            DrawerStatusUnlocked = "01";
+            ClearingInitiated = "00";
+            ClearingNotInitiated = "01";
+        }
     }
 }
