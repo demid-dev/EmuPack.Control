1b9f1dc6d46d889686e57a477a02a9ddd270d050
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:29:04 +0200

    Update StatusRequestCommand.cs

--- EmuPack.Control/Models/Commands/StatusRequestCommand.cs
+++ EmuPack.Control/Models/Commands/StatusRequestCommand.cs
@@ -5,7 +5,17 @@ using System.Threading.Tasks;
 
 namespace EmuPack.Control.Models.Commands
 {
-    public class StatusRequestCommand
+    public class StatusRequestCommand : Command
     {
+    }
+
+    static class StatusRequestCommandValues
+    {
+        static public string CommandId { get; private set; }
+
+        static StatusRequestCommandValues()
+        {
+            CommandId = "SR";
+        }
     }
 }
