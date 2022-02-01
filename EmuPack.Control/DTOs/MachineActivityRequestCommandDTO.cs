58618391974a34289e4e6612ff0d23563a83724e
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:28:46 +0200

    Create MachineActivityRequestCommandDTO.cs

---
+++ EmuPack.Control/DTOs/MachineActivityRequestCommandDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.DTOs
+{
+    public class MachineActivityRequestCommandDTO
+    {
+        public bool DrawerShouldBeLocked { get; set; }
+        public bool ClearingShouldBeInitiated { get; set; }
+    }
+}
