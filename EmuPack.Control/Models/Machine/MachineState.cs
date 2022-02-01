5086a12db7cf9d679d8af6d4542aa795a80c154a
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:29:06 +0200

    Create MachineState.cs

---
+++ EmuPack.Control/Models/Machine/MachineState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.Models.Machine
+{
+    public class MachineState
+    {
+        public List<Notification> Notifications { get; set; }
+    }
+
+    public class Notification
+    {
+        public string Description { get; set; }
+        public string Timestamp { get; set; }
+
+        public Notification()
+        {
+
+        }
+    }
+}
