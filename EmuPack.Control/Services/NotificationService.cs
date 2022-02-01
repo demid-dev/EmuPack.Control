d98555a2ac621e1ecbfec917c6462678ede1861f
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:29:10 +0200

    Create NotificationService.cs

---
+++ EmuPack.Control/Services/NotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.Services
+{
+    public class NotificationService
+    {
+        public void SendTcpConnectionErrorNotification(string ip, int port)
+        {
+
+        }
+    }
+}
