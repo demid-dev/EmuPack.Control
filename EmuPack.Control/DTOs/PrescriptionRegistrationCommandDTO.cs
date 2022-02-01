f234c4c53e0f60aade1760cf8aa6c878e0664288
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-01 18:28:49 +0200

    Create PrescriptionRegistrationCommandDTO.cs

---
+++ EmuPack.Control/DTOs/PrescriptionRegistrationCommandDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.DTOs
+{
+    public class PrescriptionRegistrationCommandDTO
+    {
+        [Range(0, 9999)]
+        public int PrescriptionId { get; set; }
+        [Required]
+        public List<RxRegistrationCommandDrugDTO> RxRegistrationCommandDrugsDTOs { get; set; }
+    }
+
+    public class RxRegistrationCommandDrugDTO
+    {
+        [Range(1, 40)]
+        public int CassetteId { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string DrugName { get; set; }
+        [Range(1, 99999)]
+        public int QuantityPerCassette { get; set; }
+    }
+}
