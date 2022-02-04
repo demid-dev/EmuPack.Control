e3ad5455e96e4acbe14de2d7527ecd40cbe88efe
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-04 17:23:31 +0200

    Create NotificationDTO.cs

---
+++ EmuPack.Control/DTOs/NotificationDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.DTOs
+{
+    public class NotificationDTO
+    {
+        public NotificationType NotificationType { get; set; }
+        public List<WarningFieldDTO> WarningFields { get; set; }
+        public string Timestamp { get; set; }
+    }
+
+    public class WarningFieldDTO
+    {
+        public string FieldName { get; set; }
+        public string Value { get; set; }
+    }
+
+    public enum NotificationType
+    {
+        TcpConnectionError,
+        TcpSendMessageError,
+        TcpReceivingError,
+        MachineBlockedCommandError,
+        CassetteWarning
+    }
+}
