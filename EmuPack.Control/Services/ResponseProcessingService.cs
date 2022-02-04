996d9bd58eac928cf85c0c3c5fa72152db88c272
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-04 17:23:58 +0200

    Create ResponseProcessingService.cs

---
+++ EmuPack.Control/Services/ResponseProcessingService.cs
@@ -0,0 +1,49 @@
+﻿using EmuPack.Control.Models.Commands;
+using EmuPack.Control.Models.Machine;
+using EmuPack.Control.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.Services
+{
+    public class ResponseProcessingService
+    {
+        private readonly NotificationService _notificationService;
+
+        public ResponseProcessingService(NotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public void ProcessResponse(string response, 
+            MachineState machineState)
+        {
+            CommandResponse commandResponse = new CommandResponse(response);
+
+            CommandResponseValues.ResponseCodes.TryGetValue(CommandResponseCodes.MachineBlockedCommand,
+                out string machineBlockedCommandCode);
+            if (commandResponse.ResponseCode == machineBlockedCommandCode)
+            {
+                _notificationService.SendMachineBlockedCommandNotification(commandResponse);
+            }
+            else if (commandResponse.CommandId == StatusRequestCommandValues.CommandId)
+            {
+                StatusCommandResponse statusCommandResponse = new StatusCommandResponse(response);
+                ProcessStatusResponse(statusCommandResponse, machineState);
+            }
+        }
+
+        private void ProcessStatusResponse(StatusCommandResponse response, 
+            MachineState machineState)
+        {
+            if (response.WarningCassettesIds.Any())
+            {
+                _notificationService.SendCassetteWarningNotification(response);
+            }
+
+            machineState.UpdateMachineState(response);
+        }
+    }
+}
