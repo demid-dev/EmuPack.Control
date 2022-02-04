26d15f7107f2e2ce177f4a74d61037d3bb3ffc53
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-04 17:23:51 +0200

    Create OperationsHandler.cs

---
+++ EmuPack.Control/Services/Operations/OperationsHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.Services.Operations
+{
+    public class OperationsHandler
+    {
+        public InitializationOperationHandler InitializationOperationHandler { get; private set; }
+        
+        public OperationsHandler()
+        {
+            InitializationOperationHandler = new InitializationOperationHandler();
+        }
+    }
+}
