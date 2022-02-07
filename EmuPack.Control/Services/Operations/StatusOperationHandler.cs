30256ce1764eed6345a76c2ace149ef6e20f4f7d
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-07 22:15:04 +0200

    Create StatusOperationHandler.cs

---
+++ EmuPack.Control/Services/Operations/StatusOperationHandler.cs
@@ -0,0 +1,13 @@
+﻿using EmuPack.Control.Models.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmuPack.Control.Services.Operations
+{
+    public class StatusOperationHandler
+    {
+
+    }
+}
