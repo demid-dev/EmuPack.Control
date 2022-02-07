4da90f8b99cb12a5d47d8b3ef65410b9a6a8bd31
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-07 22:14:59 +0200

    Update StatusCommandResponse.cs

--- EmuPack.Control/Models/Responses/StatusCommandResponse.cs
+++ EmuPack.Control/Models/Responses/StatusCommandResponse.cs
@@ -9,6 +9,8 @@ namespace EmuPack.Control.Models.Responses
     {
         public string DrawerStatus { get; private set; }
         public string AdaptorStatus { get; private set; }
+        public string RegistredPrescriptionsQuantity { get; private set; }
+        public List<string> RegistredPrescriptionsIds { get; private set; }
         public string WarningCassettesQuantity { get; private set; }
         public List<string> WarningCassettesIds { get; private set; }
 
@@ -19,15 +21,33 @@ namespace EmuPack.Control.Models.Responses
                 StatusCommandResponseValues.DrawerStatusLength);
             AdaptorStatus = response.Substring(StatusCommandResponseValues.AdaptorStartIndex,
                 StatusCommandResponseValues.AdaptorLength);
-            WarningCassettesQuantity = response.Substring(StatusCommandResponseValues.WarningCassettesQuantityStartIndex,
-                StatusCommandResponseValues.WarningCassettesQuantityLength);
 
+            RegistredPrescriptionsQuantity = response
+                .Substring(StatusCommandResponseValues.RegistredPrescriptionsQuantityStartIndex,
+                    StatusCommandResponseValues.RegistredPrescriptionsQuantityLength);
+            RegistredPrescriptionsIds = new List<string>();
+            int prescriptionsQuantity = int.Parse(RegistredPrescriptionsQuantity);
+            int warningCassettesQuantityStartIndex = StatusCommandResponseValues.RegistredPrescriptionsIdsStartIndex;
+            for (int i = 0; i < prescriptionsQuantity; i++)
+            {
+                string presriptionId = response
+                    .Substring(StatusCommandResponseValues.RegistredPrescriptionsIdsStartIndex
+                    + i * StatusCommandResponseValues.RegistredPrescriptionsIdsLength,
+                    StatusCommandResponseValues.RegistredPrescriptionsIdsLength);
+                RegistredPrescriptionsIds.Add(presriptionId);
+
+                warningCassettesQuantityStartIndex += StatusCommandResponseValues.RegistredPrescriptionsIdsLength;
+            }
+
+            WarningCassettesQuantity = response.Substring(warningCassettesQuantityStartIndex,
+                StatusCommandResponseValues.WarningCassettesQuantityLength);
             WarningCassettesIds = new List<string>();
             int cassettesQuantity = int.Parse(WarningCassettesQuantity);
             for (int i = 0; i < cassettesQuantity; i++)
             {
                 string warningCassette = response
-                    .Substring(StatusCommandResponseValues.WarningCassettesIdsStartIndex
+                    .Substring(warningCassettesQuantityStartIndex
+                    + StatusCommandResponseValues.WarningCassettesQuantityLength
                     + i * StatusCommandResponseValues.WarningCassettesIdsLength,
                     StatusCommandResponseValues.WarningCassettesIdsLength);
                 WarningCassettesIds.Add(warningCassette);
@@ -41,9 +61,11 @@ namespace EmuPack.Control.Models.Responses
         static public int DrawerStatusLength { get; private set; }
         static public int AdaptorStartIndex { get; private set; }
         static public int AdaptorLength { get; private set; }
-        static public int WarningCassettesQuantityStartIndex { get; private set; }
+        static public int RegistredPrescriptionsQuantityStartIndex { get; private set; }
+        static public int RegistredPrescriptionsQuantityLength { get; private set; }
+        static public int RegistredPrescriptionsIdsStartIndex { get; private set; }
+        static public int RegistredPrescriptionsIdsLength { get; private set; }
         static public int WarningCassettesQuantityLength { get; private set; }
-        static public int WarningCassettesIdsStartIndex { get; private set; }
         static public int WarningCassettesIdsLength { get; private set; }
         static public string DrawerOpenedValue { get; private set; }
         static public string AdaptorInDrawerValue { get; private set; }
@@ -54,9 +76,11 @@ namespace EmuPack.Control.Models.Responses
             DrawerStatusLength = 1;
             AdaptorStartIndex = 14;
             AdaptorLength = 1;
-            WarningCassettesQuantityStartIndex = 15;
+            RegistredPrescriptionsQuantityStartIndex = 15;
+            RegistredPrescriptionsQuantityLength = 2;
+            RegistredPrescriptionsIdsStartIndex = 17;
+            RegistredPrescriptionsIdsLength = 4;
             WarningCassettesQuantityLength = 2;
-            WarningCassettesIdsStartIndex = 17;
             WarningCassettesIdsLength = 2;
             DrawerOpenedValue = "1";
             AdaptorInDrawerValue = "1";
