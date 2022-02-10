e25067e2fe3b5db9aafcd92831fe259e15d365fd
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-10 18:23:13 +0200

    Update StatusOperationHandler.cs

--- EmuPack.Control/Services/Operations/StatusOperationHandler.cs
+++ EmuPack.Control/Services/Operations/StatusOperationHandler.cs
@@ -1,13 +1,42 @@
 ﻿using EmuPack.Control.Models.Commands;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EmuPack.Control.Services.Operations
 {
     public class StatusOperationHandler
     {
+        private readonly MachineClient _machineClient;
 
+        public StatusOperationHandler(MachineClient machineClient)
+        {
+            _machineClient = machineClient;
+        }
+
+        public void UpdateMachineState()
+        {
+            int lastUpdateId = _machineClient.MachineState.LastUpdateId;
+            RequestStatus();
+            WaitUntilMachineStateUpdated(lastUpdateId);
+        }
+
+        private void RequestStatus()
+        {
+            StatusRequestCommand command = new StatusRequestCommand();
+            _machineClient.SendCommand(command);
+        }
+
+        private void WaitUntilMachineStateUpdated(int idBeforeUpdate)
+        {
+            //while (idBeforeUpdate == _machineClient.MachineState.LastUpdateId)
+            //{
+            //    Debug.WriteLine(idBeforeUpdate + "|" + _machineClient.MachineState.LastUpdateId);
+            //};
+            Thread.Sleep(200);
+        }
     }
 }
