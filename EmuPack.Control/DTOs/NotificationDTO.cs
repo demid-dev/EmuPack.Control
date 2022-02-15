18a84847bfabec029ce666df750ad1f46be66318
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-15 22:34:06 +0200

    Update NotificationDTO.cs

--- EmuPack.Control/DTOs/NotificationDTO.cs
+++ EmuPack.Control/DTOs/NotificationDTO.cs
@@ -24,6 +24,7 @@ namespace EmuPack.Control.DTOs
         TcpSendMessageError,
         TcpReceivingError,
         MachineBlockedCommandError,
+        DispensingNotPossibleError,
         CassetteWarning
     }
 }
