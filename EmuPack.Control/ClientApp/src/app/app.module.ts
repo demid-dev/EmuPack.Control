a19334948bd2fee2339499cf803237610805114e
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-24 01:23:38 +0200

    Update app.module.ts

--- EmuPack.Control/ClientApp/src/app/app.module.ts
+++ EmuPack.Control/ClientApp/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { DispensingFormComponent } from './shared/components/dispensing-form/dispensing-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MainLayoutComponent,
+    DispensingFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
