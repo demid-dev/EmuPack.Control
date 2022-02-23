458bc1c2900692e57644312e0cf4b8b63b68cf5e
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-24 01:24:03 +0200

    Create dispensing-operation.ts

---
+++ EmuPack.Control/ClientApp/src/app/shared/models/dispensing-operation.ts
@@ -0,0 +1,15 @@
+export interface IDispensingOperation {
+  prescriptionId: number;
+  dispensingDrugs: IDispensingDrug[]
+}
+
+export interface IDispensingDrug {
+  cassetteId: number;
+  drugName: number;
+  usedPods: IUsedPod[]
+}
+
+export interface IUsedPod {
+  podPosition: string;
+  quantityOfDrug: number;
+}
