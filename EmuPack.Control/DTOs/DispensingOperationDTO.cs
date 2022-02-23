1bc9ff3d3438856fc7c635e7866711b3ef041a49
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2022-02-24 01:24:14 +0200

    Update DispensingOperationDTO.cs

--- EmuPack.Control/DTOs/DispensingOperationDTO.cs
+++ EmuPack.Control/DTOs/DispensingOperationDTO.cs
@@ -29,10 +29,10 @@ namespace EmuPack.Control.DTOs
     {
         [Required]
         [RegularExpression("A0|A1|A2|A3|A4|A5|A6|" +
-            "B0|B1|B2|B3|B4|B5|B6" +
-            "C0|C1|C2|C3|C4|C5|C6" +
-            "D0|D1|D2|D3|D4|D5|D6" +
-            "E0|E1|E2|E3|E4|E5|E6")]
+            "B0|B1|B2|B3|B4|B5|B6|" +
+            "C0|C1|C2|C3|C4|C5|C6|" +
+            "D0|D1|D2|D3|D4|D5|D6|" +
+            "E0|E1|E2|E3|E4|E5|E6|")]
         public string PodPosition { get; set; }
         [Range(1, 99)]
         public int QuantityOfDrug { get; set; }
